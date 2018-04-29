8d8be643002272bf92f126dd0a85e5b372c92197
William Clarke <wmmclarke@gmail.com>
2018-04-29 11:59:24 +0100

    random V1

---
+++ random/main.elm
@@ -0,0 +1,50 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+-- import Random
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { dieFace : Int }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text (toString model.dieFace) ]
+        , button [ onClick Roll ] [ text "Roll" ]
+        ]
+
+
+type Msg
+    = Roll
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Roll ->
+            ( model, Cmd.none )
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 1, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
